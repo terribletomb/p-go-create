26d98ef9f942a5112d28108e3c43c8559c343cfb
Bryan Benavides <104537354+Rainydata@users.noreply.github.com>
2025-11-17 18:59:31 -0500

    implementacion

--- tests/mocks/mock_persona_repo.go
+++ tests/mocks/mock_persona_repo.go
@@ -19,3 +19,21 @@ func (m *MockPersonaRepo) ObtenerPersonaPorDocumento(doc string) (models.Persona
 	args := m.Called(doc)
 	return args.Get(0).(models.Persona), args.Error(1)
 }
+
+func (m *MockPersonaRepo) ObtenerTodasPersonas() ([]models.Persona, error) {
+	args := m.Called()
+	if args.Get(0) == nil {
+		return nil, args.Error(1)
+	}
+	return args.Get(0).([]models.Persona), args.Error(1)
+}
+
+func (m *MockPersonaRepo) ActualizarPersona(documento string, update map[string]interface{}) error {
+	args := m.Called(documento, update)
+	return args.Error(0)
+}
+
+func (m *MockPersonaRepo) EliminarPersona(documento string) error {
+	args := m.Called(documento)
+	return args.Error(0)
+}
